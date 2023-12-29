2acdb8429e394dde09ac00e9fb1b901f59befcff
Paul Shen <pxshen@alumni.stanford.edu>
2023-12-29 13:36:08 -0500

    l

--- examples/[simulation]_periodic_array.jl
+++ examples/[simulation]_periodic_array.jl
@@ -9,12 +9,12 @@ name = "periodic_array"
 data_dir = "examples/utils"
 
 "simulation params"
-T = 10.0f0 # simulation duration in [periods]
+T = 20.0f0 # simulation duration in [periods]
 nres = 32
 Courant = 0.5f0 # Courant number
 
 "recording params"
-frameat = 1 / 4 # captures frame every _ periods
+frameat = 1 / 16 # captures frame every _ periods
 framerate = 16 # playback speed
 ########################
 
@@ -27,7 +27,7 @@ F = Float32
 name = "periodic_array"
 
 dx = 1.0f0 / nres # pixel resolution in [wavelengths]
-L = F[4, 4] # domain dimensions in [wavelengths]
+L = F[8, 8] # domain dimensions in [wavelengths]
 
 # setup FDTD
 polarization = :TMz
@@ -39,11 +39,11 @@ fdtd_configs = setup(boundaries, sources, monitors, L, dx, polarization; F, Cour
 
 ϵ1 = 1 #
 ϵ2 = 2.25f0 # 
-b = F.([norm([x, y] .- esz0 ./ 2) < 0.25 / dx for x = 1:esz0[1], y = 1:esz0[2]]) # circle
+b = F.([norm([x, y] .- esz0 ./ 2) < 1 / dx for x = 1:esz0[1], y = 1:esz0[2]]) # circle
 ϵ = ϵ2 * b + ϵ1 * (1 .- b)
 
 μ = ones(F, hsz0)
-σ = ones(F, esz0)
+σ = zeros(F, esz0)
 σm = zeros(F, hsz0)
 
 ϵ, μ, σ, σm = apply(geometry_padding; ϵ, μ, σ, σm)
@@ -52,5 +52,5 @@ u0 = collect(values(fields))
 
 # run simulation
 @showtime sol = accumulate((u, t) -> stepTMz(u, p, t, fdtd_configs), 0:dt:T, init=u0)
-recordsim(sol, p, fdtd_configs, "$(name)_nres_$nres.gif", title="$name"; frameat, framerate)
+recordsim(sol, p, fdtd_configs, "$(name)_nres_$nres.mp4", title="$name"; frameat, framerate)
 # plotmonitors(sol0, monitor_configs,)
--- examples/utils/plot_recipes.jl
+++ examples/utils/plot_recipes.jl
@@ -1,14 +1,13 @@
 using CairoMakie
 using CairoMakie: Axis
 
-function plotstep!(ax, u::AbstractArray, p::AbstractArray, configs; colorrange)
-    ϵ, μ, σ, σm = p
-    Ez, Hx, Hy = u
-    a = Ez
-    heatmap!(ax, a; colormap=:seismic, colorrange)
-    # heatmap!(ax, Array(σ), alpha=0.2, colormap=:binary, colorrange=(0, 4),)
-    heatmap!(ax, Array(ϵ), colormap=[(:white, 0), (:gray, 0.8)])#, colorrange=(ϵ1, ϵ2))
+function plotstep!(ax, u::AbstractVector, p::AbstractVector, configs; colorrange)
+    "plot field"
+    heatmap!(ax, u[1]; colormap=:seismic, colorrange)
+    "plot geometry"
+    heatmap!(ax, p[1], colormap=[(:white, 0), (:gray, 0.8)])#, colorrange=(ϵ1, ϵ2))
 
+    "plot monitors"
     marker = :rect
     for (i, m) = enumerate(configs.monitor_info)
         x, y = m.idxs
@@ -27,9 +26,7 @@ function recordsim(sol, p, fdtd_configs, fn; title="", frameat=1 / 16, framerate
     umax = maximum(map(sol) do v
         maximum(abs, v[1])
     end)
-    # umax = maximum(sol[end][1])
     r = record(fig, fn, t; framerate) do t
-        # i = round.(Int, t ./ dt) .+ 1
         i = round(Int, t / dt + 1)
         empty!(fig)
         ax = Axis(fig[1, 1]; title="t = $t\nEz\n$title")
@@ -41,16 +38,16 @@ function recordsim(sol, p, fdtd_configs, fn; title="", frameat=1 / 16, framerate
     r
 end
 
-function plotmonitors(sol, monitor_info)
+# function plotmonitors(sol, monitor_info)
 
-    t = range(0, T, length=size(sol)[end])
-    fig = Figure()
-    for i = 1:2
-        ax = Axis(fig[i, 1], title="Monitor $i")
-        E, H = get(sol, monitor_info[i],)
-        lines!(ax, t, E)
-        lines!(ax, t, H)
-        # lines!(ax, t, H .* E)
-    end
-    display(fig)
-end
+#     t = range(0, T, length=size(sol)[end])
+#     fig = Figure()
+#     for i = 1:2
+#         ax = Axis(fig[i, 1], title="Monitor $i")
+#         E, H = get(sol, monitor_info[i],)
+#         lines!(ax, t, E)
+#         lines!(ax, t, H)
+#         # lines!(ax, t, H .* E)
+#     end
+#     display(fig)
+# end
