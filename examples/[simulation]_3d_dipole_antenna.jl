1f40280008f2862e157cd2a17e302c02da0f5119
Paul Shen <pxshen@alumni.stanford.edu>
2024-01-16 21:19:12 -0500

    a

--- examples/[simulation]_3d_dipole_antenna.jl
+++ examples/[simulation]_3d_dipole_antenna.jl
@@ -11,7 +11,7 @@ dir = "examples/utils"
 
 "training params"
 T = 4.0f0 # simulation duration in [periods]
-nres = 32
+nres = 16
 dx = 1.0f0 / nres
 Courant = 0.25f0 # Courant number
 
@@ -26,9 +26,12 @@ include("../src/main.jl")
 include("../../FDTDToolkit.jl/src/plot_recipes.jl")
 
 F = Float32
-l = 1
-sz0 = l .* (nres, nres, nres)
-ϵ = ones(F, sz0)
+l = 2
+sz0 = nres .* (l, l, l)
+ϵ1 = 1 #
+ϵ2 = 2.25f0 # 
+b = F.([norm(v .- sz0 ./ 2) < 0.3 / dx for v = Base.product(Base.oneto.(sz0)...)]) # sphere
+ϵ = ϵ2 * b + ϵ1 * (1 .- b)
 
 boundaries = [] # unspecified boundaries default to PML
 boundaries = [Periodic(2), Periodic(3)]
@@ -36,7 +39,7 @@ boundaries = [Periodic(2), Periodic(3)]
 monitors = []
 sources = [
     # UniformSource(t -> cos(F(2π) * t), l/2*[1,1,1], [0, 0, 0.25f0]; Jz=1),
-    PlaneWave(t -> exp(-((t - 1) / 1)^2) * cos(F(2π) * t), -1; Jz=1)
+    PlaneWave(t -> t < 1 ? cos(F(2π) * t) : 0.0f0, -1; Jz=1)
 ]
 fdtd_configs = setup(boundaries, sources, monitors, dx, sz0; F, Courant, T)
 @unpack μ, σ, σm, dt, geometry_padding, field_padding, source_effects, monitor_instances, fields = fdtd_configs
@@ -57,7 +60,7 @@ nres = 1 ÷ dx
 E = map(sol) do u
     u[3]
 end
-recordsim(E, nothing, fdtd_configs, "$(name)_nres_$nres.mp4", title="$name"; playback=0.25, bipolar=true)
-# plotmonitors(sol0, monitor_configs,)
-display(plotstep(E[16], nothing, fdtd_configs))
+dir = @__DIR__
+recordsim(E, p[1], fdtd_configs, "$dir/$(name)_nres_$nres.mp4", title="$name"; playback=1, bipolar=true)
+# display(plotstep(E[16], nothing, fdtd_configs))
 display(volume(E[32]))
